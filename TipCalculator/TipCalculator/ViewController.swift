7a95702484997b765b98afc099516f8ec884cb9b
Andy Wong <wongandy97@gmail.com>
2017-05-16 21:28:20 -0400

    Add delegate and calculations

--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -8,16 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
 	@IBOutlet weak var balanceTextField: UITextField!
 	@IBOutlet weak var tipLabel: UILabel!
 	@IBOutlet weak var splitTextField: UITextField!
 	@IBOutlet weak var tipSlider: UISlider!
 	@IBOutlet weak var totalLabel: UILabel!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		balanceTextField.delegate = self
+		splitTextField.delegate = self
+		
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -25,6 +29,17 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
-
+	func calculateTip(Sender: AnyObject){
+		guard let balance = Double(balanceTextField.text!)
+			else{
+				balanceTextField.text = "0"
+				splitTextField.text = "0"
+				totalLabel.text = "0"
+				return
+		}
+		
+		let roundedBillAmount = round(100*balance)/100
+		let tipAmount = roundedBillAmount * //percentages(grabbed from slider or text field? 
+	}
 }
 
