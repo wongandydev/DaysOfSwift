8567811438de63c57bd71fbf227d475e39658f32
Andy Wong <wongandy97@gmail.com>
2017-05-17 21:28:34 -0400

    Add funciton to slider bar and actual calculations

--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -15,13 +15,20 @@ class ViewController: UIViewController,UITextFieldDelegate {
 	@IBOutlet weak var splitTextField: UITextField!
 	@IBOutlet weak var tipSlider: UISlider!
 	@IBOutlet weak var totalLabel: UILabel!
+	@IBOutlet weak var tipSliderResultLabel: UILabel!
 	
+	@IBAction func tipSliderMoved(_ sender: Any) {
+		let value = tipSlider.value
+		
+		tipSliderResultLabel.text = String(format: "%.2f", value) + "%"
+		calculateTip()
+	}
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 		balanceTextField.delegate = self
 		splitTextField.delegate = self
-		
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -29,17 +36,24 @@ class ViewController: UIViewController,UITextFieldDelegate {
 		// Dispose of any resources that can be recreated.
 	}
 
-	func calculateTip(Sender: AnyObject){
+	func calculateTip(){
 		guard let balance = Double(balanceTextField.text!)
 			else{
-				balanceTextField.text = "0"
+				balanceTextField.text = "$0.00"
 				splitTextField.text = "0"
-				totalLabel.text = "0"
+				totalLabel.text = "$0.00"
 				return
 		}
 		
 		let roundedBillAmount = round(100*balance)/100
-		let tipAmount = roundedBillAmount * //percentages(grabbed from slider or text field? 
+		let tipAmount = roundedBillAmount * Double(tipSlider.value)/100//percentages(grabbed from slider or text field?
+		let roundedTipAmount = round(100*tipAmount)/100
+		let total = roundedBillAmount + tipAmount
+		tipLabel.text = "$" + String(format: "%.2f", roundedTipAmount)
+		totalLabel.text = "$" + String(format: "%.2f", total)
+		
+		print(roundedBillAmount)
+		print(tipAmount)
 	}
 }
 
