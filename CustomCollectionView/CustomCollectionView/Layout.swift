2391d53e36e9c7495f4174d5bc61296ddace49ce
Andy Wong <wongandy97@gmail.com>
2017-05-23 20:17:38 -0400

    Add collectionView layout

---
+++ CustomCollectionView/CustomCollectionView/Layout.swift
@@ -0,0 +1,13 @@
+//
+//  Layout.swift
+//  CustomCollectionView
+//
+//  Created by Andy Wong on 5/23/17.
+//  Copyright © 2017 Andy Wong. All rights reserved.
+//
+
+import UIKit
+
+class Layout: UICollectionViewLayout {
+	
+}
--- CustomCollectionView/CustomCollectionView/PhotoCollectionViewController.swift
+++ CustomCollectionView/CustomCollectionView/PhotoCollectionViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PhotoCollectionViewController: UICollectionViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
