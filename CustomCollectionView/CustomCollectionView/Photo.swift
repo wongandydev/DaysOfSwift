fb5ca4da1db46ae60fc4586c27d673ee2dd0d048
Andy Wong <wongandy97@gmail.com>
2017-05-25 19:24:28 -0400

    create init files

---
+++ CustomCollectionView/CustomCollectionView/Photo.swift
@@ -0,0 +1,24 @@
+//
+//  Photo.swift
+//  CustomCollectionView
+//
+//  Created by Andy Wong on 5/25/17.
+//  Copyright © 2017 Andy Wong. All rights reserved.
+//
+
+import UIKit
+
+class Photo{
+	var caption: String
+	var comment: String
+	var image: UIImage
+	
+	init (caption: String, comment:String, image:UIImage){
+		self.caption = caption
+		self.comment = comment
+		self.image = image
+	}
+	
+	
+	
+}
