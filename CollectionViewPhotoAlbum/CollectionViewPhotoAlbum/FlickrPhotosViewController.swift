6f0496bb9e9acecdacaf63c759811bf588a21b62
Andy Wong <wongandy97@gmail.com>
2017-06-20 21:13:40 -0400

    Add flickr

---
+++ CollectionViewPhotoAlbum/CollectionViewPhotoAlbum/FlickrPhotosViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotosViewController.swift
+//  CollectionViewPhotoAlbum
+//
+//  Created by Andy Wong on 6/15/17.
+//  Copyright © 2017 Andy Wong. All rights reserved.
+//
+
+import UIKit
+
+final class FlickrPhotosViewController: UICollectionViewController{
+	fileprivate let reuseIdentifier = "FlickrCell"
+	fileprivate let sectionInsets = UIEdgeInsets(top: 50, left: 20, bottom: 50, right: 20)
+}
