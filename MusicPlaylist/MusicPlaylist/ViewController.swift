cda1d38e53b4907b01eeb9fe6ef09425e72bc12d
Andy Wong <wongandy97@gmail.com>
2017-07-01 22:07:29 -0400

    Add playlist didset method to relaoad table view

--- MusicPlaylist/MusicPlaylist/ViewController.swift
+++ MusicPlaylist/MusicPlaylist/ViewController.swift
@@ -8,8 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+	var playlists: [playlist] = []{
+		didSet{
+			tableView.reloadData()
+		}
+	}
+	
+	
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return playlists.count
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
