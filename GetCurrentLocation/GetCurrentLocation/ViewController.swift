50b8e4f900d41413c65b1b4e20b5c76933b0e8f9
Andy Wong <wongandy97@gmail.com>
2018-04-13 16:26:54 -0400

    show map at center along with full address

--- GetCurrentLocation/GetCurrentLocation/ViewController.swift
+++ GetCurrentLocation/GetCurrentLocation/ViewController.swift
@@ -21,36 +21,79 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
 
 	@IBAction func locationButtonTapped(_ sender: Any) {
 		centerUserLocation()
+		showAddressOnLabel(label: locationLabel)
 	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		self.locationManager.requestAlwaysAuthorization()
 		self.locationManager.requestWhenInUseAuthorization()
-
+		
 		if CLLocationManager.locationServicesEnabled() {
 			locationManager.delegate = self
 			locationManager.desiredAccuracy = kCLLocationAccuracyBest
 			mapView.showsUserLocation = true
 			locationManager.startUpdatingLocation()
-			centerUserLocation()
+		}
+		
+		centerUserLocation()
+		showAddressOnLabel(label: locationLabel)
+	}
+	
+	func showAddressOnLabel(label: UILabel){
+		coordinatesToAddress() { (location) in
+			if let address = location?.thoroughfare,
+				let streetNumber = location?.subThoroughfare,
+				let city = location?.locality,
+				let state = location?.administrativeArea,
+				let zipCode = location?.postalCode,
+				let country = location?.country{
+				label.text = "\(streetNumber) \(address), \(city), \(state) \(zipCode), \(country)"
+			} else {
+				print("no address")
+				label.text = "Address not available"
+			}
+		}
+	}
+	
+	func coordinatesToAddress(completionHandler: @escaping (CLPlacemark?) -> Void) {
+		if let lastLocation = locationManager.location{
+			let geocoder = CLGeocoder()
+			
+			geocoder.reverseGeocodeLocation(lastLocation, completionHandler: {(address, error) in
+				if error != nil {
+					print("error gettings address")
+					completionHandler(nil)
+				} else {
+					let firstAddress = address?[0]
+					completionHandler(firstAddress)
+				}
+			})
+		} else{
+			print("no location")
+			completionHandler(nil)
 		}
 	}
 
 	func centerUserLocation(){
 		var region = MKCoordinateRegion()
-		region.center = mapView.userLocation.coordinate
-		
+
+		if locationManager.location?.coordinate != nil{ //Too allow the app to function coorectly when using simulator since it doesn't get a locaiton.
+			region.center = (locationManager.location?.coordinate)!
+		} else {
+			region.center = mapView.userLocation.coordinate
+		}
+
 		var span = MKCoordinateSpan()
-		span.latitudeDelta = 0.05
-		span.longitudeDelta = 0.05
+		span.latitudeDelta = 0.01
+		span.longitudeDelta = 0.01
 		region.span = span
 		
 		mapView.setRegion(region, animated: true)
 	}
 
 	func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-		switch status{
+		switch status {
 		case .authorizedAlways, .authorizedWhenInUse:
 			centerUserLocation()
 		
@@ -77,7 +120,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
 		if(error != nil){
 			print("didFailWithError: \(error)")
 		}
-		
 	}
 	
 	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
